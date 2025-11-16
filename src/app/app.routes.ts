fe71c6dd127266af1fe8f919aabc1afe6fc32172
Fabio Ichimura <112282444+fichimura@users.noreply.github.com>
2025-11-16 14:18:02 -0300

    feature - create home component and remove some nx initial templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home.component/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: "DogIn - Home"
+    }
+];
